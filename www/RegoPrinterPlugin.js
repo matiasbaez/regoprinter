afc42c3a663a4fd98cee2b618902d52772dce334
Matías Báez <matiasbaez2512@gmail.com>
2021-08-07 13:58:11 -0400

    Added connect function

--- www/RegoPrinterPlugin.js
+++ www/RegoPrinterPlugin.js
@@ -1,5 +1,9 @@
 var exec = require('cordova/exec');
 
+exports.connect = function (arg0, success, error) {
+    exec(success, error, 'RegoPrinterPlugin', 'connect');
+};
+
 exports.print = function (arg0, success, error) {
     exec(success, error, 'RegoPrinterPlugin', 'print', [arg0]);
 };
